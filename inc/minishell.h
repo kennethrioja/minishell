9befdea1eae283ba85f63f272efc41b44efd470d
krioja <krioja@student.42lausanne.ch>
2022-05-18 11:28:15 +0200

    KR - added libft and typedefs

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,35 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: krioja <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/18 10:09:16 by krioja            #+#    #+#             */
+/*   Updated: 2022/05/18 11:23:17 by krioja           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
 // 42 libraries
-# include "../src/ft_printf/ft_printf.h"
-//# include "../ft_functions/libft/libft.h"
-//# include "../ft_functions/get_next_line/get_next_line.h"
+# include "../src/ft/ft_printf/ft_printf.h"
+# include "../src/ft//libft/libft.h"
 
 // .h libraries
-# include <curses.h>				// tgetent(), tgetflag(), tgetnum(), tgetstr(), tgoto(), tputs()
-# include <dirent.h>				// opendir(), readdir(), closedir()
-# include <fcntl.h>					// open()
-# include <readline/readline.h>		// readline()
-# include <readline/history.h>		// readline()
-# include <signal.h>				// signal(), sigaction(), sigemptyset(), sigaddset(), kill()
-# include <sys/ioctl.h>				// ioctl()
-# include <sys/stat.h>				// stat(), lstat(), fstat()
-# include <sys/types.h>				// read()
-# include <sys/uio.h>				// read()
-# include <sys/wait.h>				// wait(), waitpid(), wait3(), wait4()
-# include <stdio.h>					// readline(), printf(), strerror(), perror()
-# include <stdlib.h>				// malloc(), free(), exit(), getenv(), 
-# include <term.h>					// tgetent(), tgetflag(), tgetnum(), tgetstr(), tgoto(), tputs()
-# include <termios.h>				// tcsetattr(), tcgetattr()
-# include <unistd.h>				// write(), access(), read(), close(), fork(), getcwd(), chdir(), unlink(), execve(), dup(), dup2(), pipe(), isatty(), ttyname(), ttyslot()
+# include <curses.h>				
+// tgetent(), tgetflag(), tgetnum(), tgetstr(), tgoto(), tputs()
+# include <dirent.h>				
+// opendir(), readdir(), closedir()
+# include <fcntl.h>					
+// open()
+# include <readline/readline.h>		
+// readline()
+# include <readline/history.h>		
+// readline()
+# include <signal.h>				
+// signal(), sigaction(), sigemptyset(), sigaddset(), kill()
+# include <sys/ioctl.h>				
+// ioctl()
+# include <sys/stat.h>				
+// stat(), lstat(), fstat()
+# include <sys/types.h>				
+// read()
+# include <sys/uio.h>				
+// read()
+# include <sys/wait.h>				
+// wait(), waitpid(), wait3(), wait4()
+# include <stdio.h>					
+// readline(), printf(), strerror(), perror()
+# include <stdlib.h>				
+// malloc(), free(), exit(), getenv(), 
+# include <term.h>					
+// tgetent(), tgetflag(), tgetnum(), tgetstr(), tgoto(), tputs()
+# include <termios.h>				
+// tcsetattr(), tcgetattr()
+# include <unistd.h>				
+// write(), access(), read(), close(), fork(), getcwd(), chdir(),
+// unlink(), execve(), dup(), dup2(), pipe(), isatty(), ttyname(), ttyslot()
 
 // typedef, struct & vars
+
+typedef struct s_pp
+{
+	char		*cmd;
+	char		*path;
+	char		**params;
+	int			i;
+	struct s_pp	*prev;
+	struct s_pp	*next;
+}				t_pp;
+
+typedef struct s_ad
+{
+	char	*line;
+	t_pp	*pp;
+}				t_ad;
 
 // minishell functions
 
 //* prompt.c
-char	*get_line(void);
+int		get_line(t_ad *ad);
+
+//* get_path_and_params.c
 
 //* signal.c
 void	sig_handler(int signum);
